648a750c72a3c7682e086a3d0dbbab2d0a601c6b
titusdpHashMicro <titus.dirgantoro@hashmicro.com>
2021-10-26 09:26:26 +0700

    inisiasi project

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "myoutsource/",
+    'name': "My Outsource",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -16,8 +16,9 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Administration',
     'version': '0.1',
+    'aplication': True,
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -25,6 +26,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'data/data.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
